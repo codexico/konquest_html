254b718680e77be655c69de82f63fcac916221c9
Francisco Kahil <codexico@gmail.com>
2016-04-18 22:50:30 -0300

    Remove state import from fleets

--- app/scripts/modules/fleets.js
+++ app/scripts/modules/fleets.js
@@ -1,7 +1,6 @@
 import {chooseDestiny} from './utils';
-import {state} from '../main';
 
-function generateFleet(planet) {
+function generateFleet(state, planet) {
     let fleet = {};
     fleet.ships = Math.round(Math.random() * planet.ships);
     fleet.destiny = chooseDestiny(state, planet);
@@ -9,14 +8,14 @@ function generateFleet(planet) {
     return fleet;
 }
 
-export function createFleet(planet) {
-    let fleet = generateFleet(planet);
+export function createFleet(state, planet) {
+    let fleet = generateFleet(state, planet);
     planet.ships -= fleet.ships;
     state.fleets.push(fleet);
     return fleet;
 }
 
-function konquerPlanet(fleet) {
+function konquerPlanet(state, fleet) {
     let planetIndex = state.planets.indexOf(fleet.destiny);
     let planet = state.planets[planetIndex];
     let fleetPlayerIndex = state.players.indexOf(fleet.player);
@@ -34,7 +33,7 @@ function konquerPlanet(fleet) {
     planet.ships = fleet.ships;
 }
 
-function occupyPlanet(fleet) {
+function occupyPlanet(state, fleet) {
     console.log('empty planet', fleet, fleet.destiny);
     let planetIndex = state.planets.indexOf(fleet.destiny);
     let fleetPlayerIndex = state.players.indexOf(fleet.player);
@@ -45,7 +44,7 @@ function occupyPlanet(fleet) {
     state.planets[planetIndex].ships = fleet.ships;
 }
 
-function battle(fleet) {
+function battle(state, fleet) {
     // console.log('BATTLE');
     // console.log('defense=', fleet.destiny.ships, 'atack=', fleet.ships);
     let planetIndex = state.planets.indexOf(fleet.destiny);
@@ -55,7 +54,7 @@ function battle(fleet) {
         planet.ships -= fleet.ships;
     } else { // attack win
         fleet.ships -= planet.ships;
-        konquerPlanet(fleet);
+        konquerPlanet(state, fleet);
     }
 }
 
@@ -64,15 +63,15 @@ function reinforcements(planet, ships) {
     planet.ships += ships;
 }
 
-export function arrive(fleet) {
+export function arrive(state, fleet) {
     let planetIndex = state.planets.indexOf(fleet.destiny);
     let destinyPlanet = state.planets[planetIndex];
 
     if (!destinyPlanet.player) {
-        occupyPlanet(fleet);
+        occupyPlanet(state, fleet);
     } else if (fleet.player === destinyPlanet.player) {
         reinforcements(destinyPlanet, fleet.ships);
     } else {
-        battle(fleet);
+        battle(state, fleet);
     }
 }
--- app/scripts/modules/game.js
+++ app/scripts/modules/game.js
@@ -1,17 +1,22 @@
 import {logScore} from './utils';
 
 function prepateTurn(state, planets, fleets) {
+
     state.planets
     .filter(planets.isOcuppied)
     .map(planets.grow)
     .filter(planets.wishToSendFleet)
-    .map(fleets.createFleet);
+    .map((planet) => {
+        return fleets.createFleet(state, planet);
+    });
 }
 
 function executeTurn(state, fleets) {
     // todo: verify if is turn of arrival, for now all the fleets use wormholes
     // all the fleets arrive every turn
-    state.fleets.forEach(fleets.arrive);
+    state.fleets.map((fleet) => {
+        return fleets.arrive(state, fleet);
+    });
     state.fleets = [];
 }
 
