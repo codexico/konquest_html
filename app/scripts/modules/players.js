3a7252e629305763aff9dda9701a20ea47af96d5
Francisco Kahil <codexico@gmail.com>
2016-04-20 19:10:37 -0300

    Add player type

--- app/scripts/modules/players.js
+++ app/scripts/modules/players.js
@@ -2,6 +2,7 @@ import {getEmptyPlanet} from './planets';
 
 function createLife(options, planet, player) {
     planet.space.classList.add(player.name);
+    planet.space.classList.add(player.type);
     planet.ships = options.ships;
     planet.production = options.production; // override random production
     planet.player = player;
@@ -9,19 +10,21 @@ function createLife(options, planet, player) {
     player.planets.push(planet);
 }
 
-function createPlayer(state) {
+function createPlayer(state, type) {
     let player = document.createElement('span');
     player.name = 'player_' + state.players.length;
     player.className = 'player ' + player.name;
+    player.type = type;
     state.players.push(player);
     return state.players[state.players.length - 1];
 }
 
-function addPlayer(options, state) {
-    createLife(options, getEmptyPlanet(state), createPlayer(state));
+function addPlayer(options, state, type = 'computer') {
+    createLife(options, getEmptyPlanet(state), createPlayer(state, type));
 }
 
 export function addPlayers(options, state) {
+    addPlayer(options, state, 'human');
     for (let i = 0; i < options.players; i++) {
         addPlayer(options, state);
     }
